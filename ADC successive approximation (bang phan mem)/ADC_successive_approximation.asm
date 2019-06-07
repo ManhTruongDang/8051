5084ba679de4d9e477790c777396aea6a8849f55
Dang Manh Truong <dangmanhtruong@gmail.com>
2019-06-07 11:43:16 +0100

    first commit

--- ADC successive approximation (bang phan mem)/ADC_successive_approximation.asm
+++ ADC successive approximation (bang phan mem)/ADC_successive_approximation.asm
@@ -16,110 +16,110 @@ LCD_DATA_WR				EQU		1
 LCD_BUSY_RD				EQU		2
 LCD_DATA_RD				EQU		3 
 
-ORG				0h
-LJMP			MAIN
+ORG				        0h
+LJMP			        MAIN
 
 MAIN:
-MOV				ADC_OUTPUT_PORT,#0FFh	  
-MOV				A,#038h ; Khoi tao LCD 
-ACALL			COMNWRT
+MOV				        ADC_OUTPUT_PORT,#0FFh	  
+MOV				        A,#038h ; Khoi tao LCD 
+ACALL			        COMNWRT
 _loop:
 ; Bat dau chuyen doi 
-SETB			EOC
-MOV				FEEDBACK_PORT,#0h
-MOV				R0,#8 ; So lan lap
-MOV				R1,#0h ; Ket qua
-MOV				DPTR,#SAR_LIST
+SETB			        EOC
+MOV				        FEEDBACK_PORT,#0h
+MOV				        R0,#8 ; So lan lap
+MOV				        R1,#0h ; Ket qua
+MOV				        DPTR,#SAR_LIST
 _next_bit:	
-MOV				A,R0
-MOVC			A,@A+DPTR
-MOV				R2,A ; Temp
-ADD				A,R1
-MOV				FEEDBACK_PORT,A 
+MOV				        A,R0
+MOVC			        A,@A+DPTR
+MOV				        R2,A ; Temp
+ADD				        A,R1
+MOV				        FEEDBACK_PORT,A 
 ; Tao tre de doi mach so sanh
-MOV				R7,#25
+MOV				        R7,#25
 _short_delay:
-DJNZ			R7,_short_delay
+DJNZ			        R7,_short_delay
 ; Kiem tra ket qua cua mach so sanh
-JB				COMP,_continue 
-XRL				A,R2 ; Hieu chinh 
-MOV				FEEDBACK_PORT,A
+JB				        COMP,_continue 
+XRL				        A,R2 ; Hieu chinh 
+MOV				        FEEDBACK_PORT,A
 _continue:
-MOV				R1,A
-DJNZ			R0,_next_bit
+MOV				        R1,A
+DJNZ			        R0,_next_bit
 
 ; Ket thuc chuyen doi
-CLR				EOC
+CLR				        EOC
 ; Thuc hien hien thi
-MOV				A,#1h  ; Xoa man hinh hien thi
-ACALL			COMNWRT
-MOV				A,#0Eh ; Bat hien thi, nhap nhay con tro
-ACALL			COMNWRT
-MOV				A,ADC_OUTPUT_PORT ; Doc du lieu tu ADC
-CJNE			A,#0,_convert_number_for_display
-MOV				R5,#0
-MOV				R4,#0
-MOV				R3,#0
-JMP				_display_on_LCD
+MOV				        A,#1h  ; Xoa man hinh hien thi
+ACALL			        COMNWRT
+MOV				        A,#0Eh ; Bat hien thi, nhap nhay con tro
+ACALL			        COMNWRT
+MOV				        A,ADC_OUTPUT_PORT ; Doc du lieu tu ADC
+CJNE			        A,#0,_convert_number_for_display
+MOV				        R5,#0
+MOV				        R4,#0
+MOV				        R3,#0
+JMP				        _display_on_LCD
 _convert_number_for_display:
-MOV				B,#10 ; Chia cho 10
-DIV				AB
-MOV				R5,B  ; Cat so thap
-MOV				B,#10
-DIV				AB
-MOV				R4,B  ; Cat so tiep theo
-MOV				B,#10
-DIV				AB
-MOV				R3,B  ; Cat so cuoi cung   
+MOV				        B,#10 ; Chia cho 10
+DIV				        AB
+MOV				        R5,B  ; Cat so thap
+MOV				        B,#10
+DIV				        AB
+MOV				        R4,B  ; Cat so tiep theo
+MOV				        B,#10
+DIV				        AB
+MOV				        R3,B  ; Cat so cuoi cung   
 _display_on_LCD:
-MOV				DPTR,#LCD_VAL
-MOV				A,R3
-MOVC			A,@A+DPTR ; Hien thi chu so hang tram
-ACALL			DATAWRT		  
+MOV				        DPTR,#LCD_VAL
+MOV				        A,R3
+MOVC			        A,@A+DPTR ; Hien thi chu so hang tram
+ACALL			        DATAWRT		  
 
-MOV				A,R4
-MOVC			A,@A+DPTR ; Hien thi chu so hang chuc
-ACALL			DATAWRT
-ACALL			DELAY
+MOV				        A,R4
+MOVC			        A,@A+DPTR ; Hien thi chu so hang chuc
+ACALL			        DATAWRT
+ACALL			        DELAY
 
-MOV				A,R5
-MOVC			A,@A+DPTR ; Hien thi chu so hang don vi
-ACALL			DATAWRT
-ACALL			DELAY 
+MOV				        A,R5
+MOVC			        A,@A+DPTR ; Hien thi chu so hang don vi
+ACALL			        DATAWRT
+ACALL			        DELAY 
 		
-JMP				_loop 
+JMP				        _loop 
 RET	
 
 ;-------------------------------
 ; Chuong trinh con gui lenh toi LCD
  COMNWRT:
-MOV				R0,#LCD_CMD_WR
-MOVX			@R0,A
-ACALL			DELAY
+MOV				        R0,#LCD_CMD_WR
+MOVX			        @R0,A
+ACALL			        DELAY
 RET
 ;-------------------------------
 
 ;-------------------------------
 ; Chuong trinh con gui 1 byte du lieu toi LCD
 DATAWRT:
-MOV				R0,#LCD_DATA_WR
-MOVX			@R0,A
-ACALL			DELAY
+MOV				        R0,#LCD_DATA_WR
+MOVX			        @R0,A
+ACALL			        DELAY
 RET
 ;-------------------------------
 
 ;-------------------------------
 ; Chuong trinh con tao tre
 DELAY:
-MOV 			R6,#255
+MOV 			        R6,#255
 _NOP:
-MOV 			R7,#255
-DJNZ 			R7,$
-DJNZ 			R6,_NOP
+MOV 			        R7,#255
+DJNZ 			        R7,$
+DJNZ 			        R6,_NOP
 RET
 ;-------------------------------
 
-ORG			500h
-LCD_VAL:	DB		"0","1","2","3","4","5","6","7","8","9"
-SAR_LIST: DB 00000000B,00000001B,00000010B,00000100B,00001000B,00010000B,00100000B,01000000B,10000000B
+ORG			            500h
+LCD_VAL:	            DB		"0","1","2","3","4","5","6","7","8","9"
+SAR_LIST:               DB 00000000B,00000001B,00000010B,00000100B,00001000B,00010000B,00100000B,01000000B,10000000B
 END
